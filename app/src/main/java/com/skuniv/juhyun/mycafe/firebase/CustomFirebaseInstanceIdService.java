4d3314e9e03113fe8a83d1190775bfdd962a57c8
KimJuhyun92 <juhyun7793@gmail.com>
2018-06-09 15:36:44 +0900

    first commit

---
+++ app/src/main/java/com/skuniv/juhyun/mycafe/firebase/CustomFirebaseInstanceIdService.java
@@ -0,0 +1,10 @@
+package com.skuniv.juhyun.mycafe.firebase;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Juhyun on 2018-06-07.
+ */
+
+public class CustomFirebaseInstanceIdService extends FirebaseInstanceIdService {
+}
---
+++ app/src/main/java/com/skuniv/juhyun/mycafe/firebase/CustomFirebaseMessagingService.java
@@ -0,0 +1,11 @@
+package com.skuniv.juhyun.mycafe.firebase;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by Juhyun on 2018-06-07.
+ */
+
+public class CustomFirebaseMessagingService extends FirebaseMessagingService {
+
+
+}
